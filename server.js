b736a187686de3afdd7fadc796d649b0795d6722
Rachana <zimboganyo@gmail.com>
2023-02-27 13:41:57 -0700

    Monday

--- server.js
+++ server.js
@@ -73,9 +73,9 @@ app.post('/login', async(req,res) =>{
 
 https.createServer(
     {
-        key:fs.readFileSync('./server.key'),
-        cert:fs.readFileSync('./server.cert'), 
-        ca:fs.readFileSync('./chain.pem')
+        key:fs.readFileSync('/etc/letsencrypt/live/rachanakumbhakar.cit270.com/privkey.pem'),
+        cert:fs.readFileSync('/etc/letsencrypt/live/rachanakumbhakar.cit270.com/cert.pem'), 
+        ca:fs.readFileSync('/etc/letsencrypt/live/rachanakumbhakar.cit270.com/fullchain.pem')
     },
 app
 ).listen(port, () =>{
